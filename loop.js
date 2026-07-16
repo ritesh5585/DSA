48b0edd2996ccedbde80636a1f77b9c95e2209d6
ritesh vishwakarma <vishwakarmaritesh5585@gmail.com>
2026-07-16 23:15:49 +0530

    Calculate the power of a Number

--- loop.js
+++ loop.js
@@ -16,13 +16,32 @@ let n = Number(prompt("Give your number here "))
 
 // =========================================================
 
-function isPrime(n) {
-    if (n < 2) return "Not Prime"
+// function isPrime(n) {
+//     if (n < 2) return "Not Prime"
     
-    for (let i = 2; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) return "Not Prime"
-    }
-    return "Prime"
-}
+//     for (let i = 2; i <= Math.sqrt(n); i++) {
+//         if (n % i === 0) return "Not Prime"
+//     }
+//     return "Prime"
+// }
 
-console.log(isPrime(n))  
+// console.log(isPrime(n))  
+
+//================================================================
+
+// function calculatePower(a, b) {
+//     // Write your logic here
+//     // 0^0 is Undefined
+//     if (a === 0 && b === 0) return "Undefined"; 
+    
+//     // Loop for exponent
+//     let result = 1;
+//     for (let i = 0; i < Math.abs(b); i++) { 
+//         result *= a;
+//     }
+    
+//     return result;
+// }
+    
+
+// module.exports = { calculatePower };

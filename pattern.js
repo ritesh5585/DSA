f235511d7f4bf5303f5be38b6c7a4d988cfbe9ce
ritesh vishwakarma <vishwakarmaritesh5585@gmail.com>
2026-08-01 22:35:10 +0530

    Comment out unused pattern printing sections and clarify inverted triangle star pattern

--- pattern.js
+++ pattern.js
@@ -15,10 +15,20 @@ let n = Number(prompt("Give your number here "))
 //     }
 //     console.log()
 // }
-for (let i = 1; i <= n; i++) {
+// for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= i; j++) {
+//         let letter = String.fromCharCode(64 + j)
+//         process.stdout.write(letter + " ")
+//     } console.log()
+// }
+
+// Inverted Right Triangle -
+//     Star Pattern
+
+for (let i = n; i >= 1; i--) {
     for (let j = 1; j <= i; j++) {
-        let letter = String.fromCharCode(64 + j)
-        process.stdout.write(letter + " ")
-    } console.log()
+        process.stdout.write("* ");
+    }
+    console.log();
 }
 

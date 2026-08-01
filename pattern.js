b8015d4d43ed84e59d8fcb8fa65e7594089adbe1
ritesh vishwakarma <vishwakarmaritesh5585@gmail.com>
2026-08-01 22:12:49 +0530

    Modify pattern printing to display numbers in a triangular format

--- pattern.js
+++ pattern.js
@@ -1,9 +1,19 @@
 let prompt = require("prompt-sync")()
 let n = Number(prompt("Give your number here "))
 
+//right anglie triangle start pattern & number
+
+// for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= i; j++) {
+//         process.stdout.write("* ")
+//     }
+//     console.log()
+// }
 for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= i; j++) {
-        process.stdout.write("* ")
+        process.stdout.write(String(j + " "))
     }
     console.log()
 }
+
+
